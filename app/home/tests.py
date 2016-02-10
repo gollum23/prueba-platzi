a4270eea7c6ab5887d08f4ba34f4e6a7c3c51b55
Diego Forero <gollum23@gmail.com>
2016-02-10 14:43:31 -0500

    Include cases pending testing

--- app/home/tests.py
+++ app/home/tests.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
 from django.test import TestCase
@@ -36,3 +37,19 @@ class LoginTest(TestCase):
     # Test login with subscription active (between thirty days from last payment)
 
     # Test login with subscription defeated (after thirty one days from last payment)
+
+
+class SubscribeTest(TestCase):
+
+    def setUp(self):
+        self.card_number_correct = 4242424242424242
+        self.card_csv_correct = 123
+        self.card_number_incorrect = 4242424242424243
+        self.card_csv_incorrect = 124
+        self.card_month = 6
+        self.card_year = 2020
+
+    # Test create user with data correct
+
+    # Test create user with data incorrect
+
