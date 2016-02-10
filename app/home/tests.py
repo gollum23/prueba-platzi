742df4b71344c597a693585531d7281f5c4cc8f2
Diego Forero <gollum23@gmail.com>
2016-02-10 11:15:29 -0500

    Create tests for login cases

--- app/home/tests.py
+++ app/home/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
 from django.test import TestCase
 
@@ -13,3 +14,21 @@ class BasicTest(TestCase):
         # Test if subscribe url exist
         res = self.client.get(reverse_lazy('home:subscribe'))
         self.assertEqual(res.status_code, 200)
+
+
+class LoginTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='platzi', email='gollum23@gmail.com', password='platzi2016')
+
+    # Test login with incorrect credential username
+    def test_login_incorrect_username(self):
+        self.assertEqual(self.client.login(username='gollum23', password='platzi2016'), False)
+
+    # Test login with incorrect credential pass
+    def test_login_incorrect_password(self):
+        self.assertEqual(self.client.login(username='platzi', password='platzi'), False)
+
+    # Test login with correct credentials
+    def test_login_correct_data(self):
+        self.assertTrue(self.client.login(username='platzi', password='platzi2016'))
