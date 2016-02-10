c2581a10aee0466f2e7a9dfb8ec08c9e3c57d56a
Diego Forero <gollum23@gmail.com>
2016-02-10 08:38:41 -0500

    Create urls for test

---
+++ app/home/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from .views import HomeView, SubscribeView
+
+urlpatterns = [
+    url(r'$', HomeView.as_view(), name='home'),
+    url(r'suscripcion/$', SubscribeView.as_view(), name='subscribe')
+]
