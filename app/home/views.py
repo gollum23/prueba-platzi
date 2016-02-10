9cc21fa3cfc2cb7fe9050192a14ec8a851771ad5
Diego Forero <gollum23@gmail.com>
2016-02-10 08:38:26 -0500

    Create basic TemplatesView for test

--- app/home/views.py
+++ app/home/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+class SubscribeView(TemplateView):
+    template_name = 'subscribe.html'
