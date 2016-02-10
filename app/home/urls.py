56fabf7c92bd06921b7f2d55f59558b17dc0ef6c
Diego Forero <gollum23@gmail.com>
2016-02-10 14:35:26 -0500

    Fix url error, missing '^'

--- app/home/urls.py
+++ app/home/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import HomeView, SubscribeView
 
 urlpatterns = [
-    url(r'$', HomeView.as_view(), name='home'),
-    url(r'suscripcion/$', SubscribeView.as_view(), name='subscribe')
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^suscripcion/$', SubscribeView.as_view(), name='subscribe')
 ]
