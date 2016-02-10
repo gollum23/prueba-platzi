b7e48c18c32a29a8af521eea214e7eac9cb8e770
Diego Forero <gollum23@gmail.com>
2016-02-10 08:39:09 -0500

    Create basic test

--- app/home/tests.py
+++ app/home/tests.py
@@ -1,3 +1,15 @@
+from django.core.urlresolvers import reverse_lazy
 from django.test import TestCase
 
-# Create your tests here.
+
+class BasicTest(TestCase):
+
+    def test_views(self):
+
+        # Test if home url exist
+        res = self.client.get(reverse_lazy('home:home'))
+        self.assertEqual(res.status_code, 200)
+
+        # Test if subscribe url exist
+        res = self.client.get(reverse_lazy('home:subscribe'))
+        self.assertEqual(res.status_code, 200)
