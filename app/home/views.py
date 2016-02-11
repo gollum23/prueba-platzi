9e456f2a8f718c55190ebe09a55c26db501c9e95
Diego Forero <gollum23@gmail.com>
2016-02-11 14:42:09 -0500

    Start implement stripe api in view

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView, CreateView
 import stripe
 
+from .models import SubscriptionUserData
 from .forms import SubscriptionForm
 
 
@@ -12,6 +16,7 @@ class HomeView(TemplateView):
 class SubscribeView(CreateView):
     template_name = 'subscribe.html'
     form_class = SubscriptionForm
+    stripe.api_key = settings.STRIPE_SECRET_KEY
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
@@ -24,9 +29,38 @@ class SubscribeView(CreateView):
         card_year = form.cleaned_data['card_year']
         card_cvc = form.cleaned_data['card_cvc']
 
-        user = User.objects.create_user(
-            username=username, email=email, password=password)
+        try:
+            stripe_token = stripe.Token.create(
+                card={
+                    'number': card_number,
+                    'exp_month': card_month,
+                    'exp_year': card_year,
+                    'cvc': card_cvc
+                }
+            )
+        except:
+            stripe_token = None
 
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
+        customer = stripe.Customer.create(
+            card=stripe_token,
+            email=email,
+            description='Suscription test',
+            plan='montlhy'
+        )
+
+        if customer.id:
+            user = User.objects.create_user(
+                username=username, email=email, password=password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+
+            payment_date = datetime.utcfromtimestamp(self.customer.created)
+            amount = self.customer.subscriptions.data[0].plan.amount/100
+
+            SubscriptionUserData.objects.create(
+                user=user,
+                payment_date=payment_date,
+                amount=amount,
+                stripe_id=customer.id
+            )
