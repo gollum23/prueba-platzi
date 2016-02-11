426bd22368787af32d919623046fc4b4ee6bc66f
Diego Forero <gollum23@gmail.com>
2016-02-11 14:41:41 -0500

    Create new test validating api stripe

--- app/home/tests.py
+++ app/home/tests.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
 from django.test import TestCase
+import stripe
+
+from .models import SubscriptionUserData
 
 
 class BasicTest(TestCase):
@@ -43,13 +47,142 @@ class SubscribeTest(TestCase):
 
     def setUp(self):
         self.card_number_correct = 4242424242424242
-        self.card_csv_correct = 123
+        self.card_cvc_correct = 123
         self.card_number_incorrect = 4242424242424243
-        self.card_csv_incorrect = 124
+        self.card_cvc_incorrect = 124
         self.card_month = 6
         self.card_year = 2020
+        stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # Test create user with data correct
+    # Test get token from stripe api with incorrect data
+    def test_create_token_with_incorrect_data_and_return_error(self):
+        try:
+            stripe.Token.create(
+                card={
+                    'number': self.card_number_incorrect,
+                    'exp_month': self.card_month,
+                    'exp_year': self.card_year,
+                    'cvc': self.card_cvc_correct
+                }
+            )
 
-    # Test create user with data incorrect
+            error_message = None
+        except stripe.error.CardError as e:
+            error_message = e.code
+
+        self.assertTrue(error_message)
+
+    # Test get token form stripe api with correct data
+    def test_create_token_with_correct_data_and_return_stripe_token(self):
+
+        try:
+            stripe_token = stripe.Token.create(
+                card={
+                    'number': self.card_number_correct,
+                    'exp_month': self.card_month,
+                    'exp_year': self.card_year,
+                    'cvc': self.card_cvc_correct
+                }
+            )
+            token = stripe_token
+        except stripe.error:
+            token = None
+
+        self.assertTrue(token)
+
+    # Test make payment y get customer id
+    def test_payment_and_return_customer_id(self):
+        stripe_token = stripe.Token.create(
+            card={
+                'number': self.card_number_correct,
+                'exp_month': self.card_month,
+                'exp_year': self.card_year,
+                'cvc': self.card_cvc_correct
+            }
+        )
+
+        customer = stripe.Customer.create(
+            card=stripe_token,
+            email='g@platzi.com',
+            description='Suscription test',
+            plan='montlhy'
+        )
+
+        self.assertTrue(customer.id)
+
+
+class UserTest(TestCase):
+
+    def setUp(self):
+        self.card_number_correct = 4242424242424242
+        self.card_cvc_correct = 123
+        self.card_number_incorrect = 4242424242424243
+        self.card_cvc_incorrect = 124
+        self.card_month = 6
+        self.card_year = 2020
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
+        self.stripe_token = stripe.Token.create(
+            card={
+                'number': self.card_number_correct,
+                'exp_month': self.card_month,
+                'exp_year': self.card_year,
+                'cvc': self.card_cvc_correct
+            }
+        )
+
+        self.customer = stripe.Customer.create(
+            card=self.stripe_token,
+            email='g@platzi.com',
+            description='Suscription test',
+            plan='montlhy'
+        )
+
+    # Test create user with customer id
+    def test_save_user_basic_and_stripe_data(self):
+
+        username = 'platzi'
+        password = 'platzi2016'
+        email = 'g@platzi.com'
+        first_name = 'diego'
+        last_name = 'forero'
+        customer_id = self.customer.id
+        payment_date = datetime.utcfromtimestamp(self.customer.created)
+        print(payment_date)
+        amount = self.customer.subscriptions.data[0].plan.amount/100
+        print(amount)
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+
+        subscription = SubscriptionUserData.objects.create(
+            user=user,
+            payment_date=payment_date,
+            amount=amount,
+            stripe_id=customer_id
+        )
+
+        # Test user exist
+        self.assertTrue(User.objects.get(username='platzi'))
+
+        # Test subscription exist
+        get_user = User.objects.get(username='platzi')
+        self.assertTrue(SubscriptionUserData.objects.filter(user=get_user))
+
+    # Test post method create subscription
+    def test_save_post_method(self):
+        req = self.client.post('/suscripcion/', data={
+            'username': 'platzi',
+            'password': 'platzi2016',
+            'email': 'g@platzi.com',
+            'first_name': 'diego',
+            'last_name': 'forero',
+            'customer_id': self.customer.id,
+            'payment_date': datetime.utcfromtimestamp(self.customer.created),
+            'amount': self.customer.subscriptions.data[0].plan.amount/100
+        })
+
+        self.assertEqual(req.status_code, 200)
 
