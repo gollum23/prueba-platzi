9bd2988b178db97509a990b3c9ef2ebe3021e971
Diego Forero <gollum23@gmail.com>
2016-02-12 20:28:51 -0500

    Include url for success subscribe view

--- app/home/urls.py
+++ app/home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import HomeView, SubscribeView
+from .views import HomeView, SubscribeView, SubscribeSuccessView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^suscripcion/$', SubscribeView.as_view(), name='subscribe')
+    url(r'^suscripcion/$', SubscribeView.as_view(), name='subscribe'),
+    url(r'^exito/$', SubscribeSuccessView.as_view(), name='success'),
 ]
